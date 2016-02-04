9e25fe5f9e0e06c2dc25c58e4167d91d159d8ff8
Sang Yu <leveraon@gmail.com>
2016-02-03 21:23:12 -0800

    Add angular support
    add thyemeleaf support

--- springmvc-restful/src/main/java/com/leveraon/springmvc/restful/config/HelloWorldConfiguration.java
+++ springmvc-restful/src/main/java/com/leveraon/springmvc/restful/config/HelloWorldConfiguration.java
@@ -3,10 +3,16 @@ package com.leveraon.springmvc.restful.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.leveraon.springmvc.restful")
-public class HelloWorldConfiguration {
-
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
 }
---
+++ springmvc-restful/src/main/java/com/leveraon/springmvc/restful/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.leveraon.springmvc.restful.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("recipient", "World");
+		return "index";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getIndexPage() {
+		return "UserManagement";
+	}
+}
